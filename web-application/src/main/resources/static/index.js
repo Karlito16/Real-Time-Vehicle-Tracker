541880b2afa599a074d7a7524e9a764a603092c2
Karlito <38817922+Karlito16@users.noreply.github.com>
2025-02-02 11:12:09 +0100

    Update index.js

--- web-application/src/main/resources/static/index.js
+++ web-application/src/main/resources/static/index.js
@@ -32,7 +32,6 @@ const getToken = async () => {
 
 
 const main = async () => {
-    // var token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZW5hbnRAdGhpbmdzYm9hcmQub3JnIiwidXNlcklkIjoiOGVkMDNjNTAtZGU3Yi0xMWVmLWIzMTUtZjliMzc0ZDlmYmY3Iiwic2NvcGVzIjpbIlRFTkFOVF9BRE1JTiJdLCJzZXNzaW9uSWQiOiJhMTU1MWMzYS1lNWIwLTQzODEtYTE1NS1kMTBiMzhhYjBmZjEiLCJleHAiOjE3MzgxOTk0MDgsImlzcyI6InRoaW5nc2JvYXJkLmlvIiwiaWF0IjoxNzM4MTkwNDA4LCJlbmFibGVkIjp0cnVlLCJpc1B1YmxpYyI6ZmFsc2UsInRlbmFudElkIjoiOGU5MzVhNjAtZGU3Yi0xMWVmLWIzMTUtZjliMzc0ZDlmYmY3IiwiY3VzdG9tZXJJZCI6IjEzODE0MDAwLTFkZDItMTFiMi04MDgwLTgwODA4MDgwODA4MCJ9.2gi_Oyb3EwqNpJX5se9iD_YJkI1M3BrjOcFMXm8TFF8TYmdhoKyf-jRpAxCgoUWquzp2QJah1Py9ev0iJx50Hg";
     const token = await getToken();
     const entityId = "8f0e2fb0-de7b-11ef-b315-f9b374d9fbf7";
     const webSocket = new WebSocket("ws://localhost:8080/api/ws");
